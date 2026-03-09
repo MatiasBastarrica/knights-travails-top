b2f8c9f4c2002f88fad23ca896b0bee67352a9e2
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-09 11:29:57 -0300

    Get the knight's valid moves given a starting position

---
+++ knight.js
@@ -0,0 +1,41 @@
+function getValidMoves(startVertex) {
+  let validMoves = [];
+  let x = startVertex[0];
+  let y = startVertex[1];
+
+  if (x + 2 <= 7) {
+    if (y + 1 <= 7) {
+      validMoves.push([x + 2, y + 1]);
+    }
+    if (y - 1 >= 0) {
+      validMoves.push([x + 2, y - 1]);
+    }
+  }
+
+  if (x - 2 >= 0) {
+    if (y + 1 <= 7) {
+      validMoves.push([x - 2, y + 1]);
+    }
+    if (y - 1 >= 0) {
+      validMoves.push([x - 2, y - 1]);
+    }
+  }
+
+  if (y + 2 <= 7) {
+    if (x + 1 <= 7) {
+      validMoves.push([x + 1, y + 2]);
+    }
+    if (x - 1 >= 0) {
+      validMoves.push([x - 1, y + 2]);
+    }
+  }
+
+  if (y - 2 <= 7) {
+    if (x + 1 <= 7) {
+      validMoves.push([x + 1, y - 2]);
+    }
+    if (x - 1 >= 0) {
+      validMoves.push([x - 1, y - 2]);
+    }
+  }
+}
