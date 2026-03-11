2592e6e0cd738611334dcc7bbace14e8ef0410be
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-10 21:43:23 -0300

    Import "knightMoves" into the main.js module

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import { getValidMoves } from "./knight.js";
+import { getValidMoves, knightMoves } from "./knight.js";
 
-console.log(getValidMoves([3, 3]));
-console.log(getValidMoves([4, 5]));
-console.log(getValidMoves([2, 4]));
+// console.log(getValidMoves([3, 3]));
+// console.log(getValidMoves([4, 5]));
+// console.log(getValidMoves([2, 4]));
