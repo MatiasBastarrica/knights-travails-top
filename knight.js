0b663b451363fc4f2dbd6f8f61cee8174291c544
MatiasBastarrica <205562029+MatiasBastarrica@users.noreply.github.com>
2026-03-11 12:00:53 -0300

    Use a Queue instead of an Array to store the valid moves

--- knight.js
+++ knight.js
@@ -1,47 +1,57 @@
 import { Queue } from "./queue.js";
 
 export function getValidMoves(startVertex) {
-  let validMoves = [];
+  let queue = new Queue();
+  // let validMoves = [];
   let x = startVertex[0];
   let y = startVertex[1];
 
   if (x + 2 <= 7) {
     if (y + 1 <= 7) {
-      validMoves.push([x + 2, y + 1]);
+      // validMoves.push([x + 2, y + 1]);
+      queue.enqueue([x + 2, y + 1]);
     }
     if (y - 1 >= 0) {
-      validMoves.push([x + 2, y - 1]);
+      // validMoves.push([x + 2, y - 1]);
+      queue.enqueue([x + 2, y - 1]);
     }
   }
 
   if (x - 2 >= 0) {
     if (y + 1 <= 7) {
-      validMoves.push([x - 2, y + 1]);
+      // validMoves.push([x - 2, y + 1]);
+      queue.enqueue([x - 2, y + 1]);
     }
     if (y - 1 >= 0) {
-      validMoves.push([x - 2, y - 1]);
+      // validMoves.push([x - 2, y - 1]);
+      queue.enqueue([x - 2, y - 1]);
     }
   }
 
   if (y + 2 <= 7) {
     if (x + 1 <= 7) {
-      validMoves.push([x + 1, y + 2]);
+      // validMoves.push([x + 1, y + 2]);
+      queue.enqueue([x + 1, y + 2]);
     }
     if (x - 1 >= 0) {
-      validMoves.push([x - 1, y + 2]);
+      // validMoves.push([x - 1, y + 2]);
+      queue.enqueue([x - 1, y + 2]);
     }
   }
 
   if (y - 2 <= 7) {
     if (x + 1 <= 7) {
-      validMoves.push([x + 1, y - 2]);
+      // validMoves.push([x + 1, y - 2]);
+      queue.enqueue([x + 1, y - 2]);
     }
     if (x - 1 >= 0) {
-      validMoves.push([x - 1, y - 2]);
+      // validMoves.push([x - 1, y - 2]);
+      queue.enqueue([x - 1, y - 2]);
     }
   }
 
-  return validMoves;
+  // return validMoves;
+  return queue;
 }
 
 export function knightMoves(start, end, visited = []) {
--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 import { getValidMoves, knightMoves } from "./knight.js";
 
-// console.log(getValidMoves([3, 3]));
+console.log(getValidMoves([3, 3]));
 // console.log(getValidMoves([4, 5]));
 // console.log(getValidMoves([2, 4]));
+
+// console.log(knightMoves([3, 3], [3, 4]));
